d16ddcc703c7d44701f194620839736ddea2b965
polypmer <exorable.ludos@gmail.com>
2017-03-24 09:18:23 -0400

    Added tabs

--- cutter.cpp
+++ cutter.cpp
@@ -5,19 +5,34 @@
 #include <poppler-document.h>
 #include <poppler-page.h>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
 #include <QtWidgets/QLabel>
 #include <QtGui/QImage>
 #include <QPixmap>
+#include <QSize>
 
 //#include <QGraphicsScene>
 
 int main(int argc, char **argv) {
   // Must have!
   QApplication app (argc, argv);
+  QMainWindow *window = new QMainWindow();
+  window->setWindowTitle(QString::fromUtf8("CookieCutter"));
+  QWidget *centralWidget = new QWidget(window);
+  QTabWidget *tabs = new QTabWidget(centralWidget);
 
+  //tabs->setFixedSize(495, 495);
+  //tabs->addTab(new QWidget(),"TAB 1");
+  //
   // Load the document into a poppler:document
   Poppler::Document* document = Poppler::Document::load(argv[1]);
-  //poppler::document* doc = poppler::document::load_from_file(argv[1]);
+  Poppler::Page *page = document->page(0);
+  QSize dims = page->pageSize();
+
+  window->resize(dims.width(),dims.height());
+
+
+
   if (!document) {
     std::cout << "No Load" << std::endl;
   }
@@ -32,10 +47,14 @@ int main(int argc, char **argv) {
       // sets parent of label to main window
       QLabel *label = new QLabel("Image");
       label->setPixmap(QPixmap::fromImage(image));
-      // label->show();
+      tabs->addTab(label,"tab");
       delete page;
     }
   }
+
+  window->setCentralWidget(centralWidget);
+  window->show();
+
 
   return app.exec();
 }
