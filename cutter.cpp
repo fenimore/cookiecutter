a1b1bd6ca7d472c3efaf9612eb83043f5e477c72
polypmer <exorable.ludos@gmail.com>
2017-03-24 20:57:10 -0400

    Add Point struct

--- cutter.cpp
+++ cutter.cpp
@@ -4,31 +4,73 @@
 #include <poppler-qt5.h>
 #include <poppler-document.h>
 #include <poppler-page.h>
+#include <poppler/cpp/poppler-rectangle.h>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QLabel>
 #include <QtGui/QImage>
 #include <QPixmap>
+#include <QPainter>
 #include <QSize>
 #include <QDebug>
 #include <QMouseEvent>
 
+#include <vector>
+
+using namespace std;
+
+struct Point {
+  int x, y;
+  //Point();
+  Point New(int, int);
+};
+
+Point NewPoint(int a, int b) {
+  Point result = Point();
+  result.x = a;
+  result.y = b;
+  return result;
+}
+
+vector<Point> NewBox(Point a, Point z) {
+  Point x = a;
+  Point y = z;
+  Point w = NewPoint(z.x, a.y);
+  Point h = NewPoint(a.x, z.y);
+  return vector<Point>{x, y, w, h};
+}
+
 class PageLabel : public QLabel {
-  //  Q_OBJECT
+  //Q_OBJECT
 public:
   PageLabel(QWidget* parent=0);
   void mousePressEvent(QMouseEvent * ev);
+  // void paintEvent(QPaintEvent * e);
+  Point lastVec;
 };
 
 PageLabel::PageLabel(QWidget* parent) : QLabel(parent) {
 }
 
-void PageLabel::mousePressEvent(QMouseEvent *ev)
-{
-  QString x = QString::number(ev->x());
-  QString y = QString::number(ev->y());
-  qDebug() << x << "," << y;
+void PageLabel::mousePressEvent(QMouseEvent *ev) {
+  qDebug() << lastVec.x << "," << lastVec.y;
+  int x = ev->x();
+  int y = ev->y();
+  Point nextVec = NewPoint(x, y);
+  vector<Point> rec = NewBox(lastVec, nextVec);
+  // cout << rec[0].x << " " << rec[1].y << " "  << " " << endl;
+  lastVec.x = x;
+  lastVec.y = y;
 }
+
+// void PageLabel::paintEvent(QPaintEvent *e) {
+//   QPainter painter(this);
+//   QRectF rectangle(10.0, 20.0, 80.0, 60.0);
+//   int startAngle = 30 * 16;
+//   int spanAngle = 120 * 16;
+//   painter.drawArc(rectangle, startAngle, spanAngle);
+// }
+
 
 int main(int argc, char **argv) {
   // Must have!
